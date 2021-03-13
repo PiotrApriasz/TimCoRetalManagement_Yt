c66cba8c39c73f877cb23a8f4ed7018e3934efec
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-03-13 22:45:20 +0100

    Add MVVM folder structure. Use caliburn.micro functionality
    Add ShellViewModel and ShellView
    Create Bootstrapper which uses caliburn.micro to initialize new window from views using viewmodel

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    /// <summary>
+    /// Setup caliburn.micro
+    /// </summary>
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Launch Shell view model as base view
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
