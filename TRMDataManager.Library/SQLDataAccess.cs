245e52ee8f127d4774c8753841f1e35b19a9c17d
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-04-11 23:14:11 +0200

    Add code to return connection string
    Install dapper to perform data operation

--- TRMDataManager.Library/SQLDataAccess.cs
+++ TRMDataManager.Library/SQLDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace TRMDataManager.Library
 {
     public class SQLDataAccess
     {
-
+        public string GetConnectionString(string name)
+        {
+            //Go to the web.config or app config, gets the conection string with the matching name
+            //and returns that connection string
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
     }
 }
