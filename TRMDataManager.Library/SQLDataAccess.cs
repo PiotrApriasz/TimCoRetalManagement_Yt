3e0d6d28956b19248f6e7c8579d746cf458c4393
Piotr Apriasz <piotrekapriasz@gmail.com>
2021-04-11 23:30:04 +0200

    Add function to load data from sql database using Dapper

--- TRMDataManager.Library/SQLDataAccess.cs
+++ TRMDataManager.Library/SQLDataAccess.cs
@@ -1,19 +1,49 @@
-﻿using System;
+﻿using Dapper;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TRMDataManager.Library
 {
     public class SQLDataAccess
     {
+        /// <summary>
+        /// Get connection string based on matching name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public string GetConnectionString(string name)
         {
             //Go to the web.config or app config, gets the conection string with the matching name
             //and returns that connection string
             return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        /// <summary>
+        /// Load data from the database
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="U"></typeparam>
+        /// <param name="storeProcedure"></param>
+        /// <param name="parameters"></param>
+        /// <param name="connectionStringName"></param>
+        /// <returns></returns>
+        public List<T> LoadData<T, U>(string storeProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            //Makes connection to database
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                //Returns set of rows
+                List<T> rows = connection.Query<T>(storeProcedure, parameters,
+                        commandType: CommandType.StoredProcedure)
+                        .ToList();
+
+                return rows;
+            }
         }
     }
 }
